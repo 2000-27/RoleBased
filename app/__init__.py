204230491a7a92c4b4239928545c103d94ffb3dc
Neetu <jangid2000neetu@gmail.com>
2023-09-20 15:47:38 +0530

    model for user and role

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+from .config import DevConfig
 
 
+db = SQLAlchemy()
+ma = Marshmallow()
 
 app = Flask(__name__)
+app.config.from_object(DevConfig)
